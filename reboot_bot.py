21bc25ebfd64a9d920c20ca0e1def152570cc26c
chl3250 <68487106+chl3250@users.noreply.github.com>
2020-07-19 10:19:26 +0900

    Add files via upload

---
+++ reboot_bot.py
@@ -0,0 +1,23 @@
+import discord
+import asyncio
+from os import system
+import time
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("파이브엠 서버 리붓봇이 가동되었습니다!")
+    @client.event
+    async def on_message(message):리붓중. . . . 
+        channel = message.channel.id
+        id = message.author.id
+        if message.content.startswith("/리붓"):
+                await message.channel.send("리붓을 시작합니다. . .")
+                system("taskkill /f /im cmd.exe")
+                system("taskkill /f /im FXServer.exe")
+                time.sleep(1)
+                system("start Start_Server.cmd")
+                time.sleep(20)
+                await message.channel.send("엘로스서버의 리붓이 완료되었습니다!")
+client.run("NzI5MjU4NjI0NDIxMTk5ODky.XxOcBg.y7WUDhm_WHEyF1ys8ta0Ap_KVAY")
